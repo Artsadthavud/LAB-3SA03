55ba4e44f3f33b3b1eaee1022e4ef7978a9bbd36
Artsadthavud <56318713+Artsadthavud@users.noreply.github.com>
2020-08-16 00:31:35 +0700

    เพิ่ม card styling พร้อมรูปแบบการแสดงแบบ array

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import CharacterCard from './CharacterCard';
 class App extends Component {
+   word = "Hello";
  render() {
  return (
  <div className="App">
-      <CharacterCard value = "h"/>
-      <CharacterCard value = "i"/>
+   {
+     Array.from(this.word).map((c, i) => <CharacterCard value = {c} key={i}/>)
+   }
+      
  </div>
  );
  }
