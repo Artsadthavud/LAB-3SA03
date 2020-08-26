0cd9edb5843ef0a7af90961f81479c846bb7d57a
Artsadthavud <56318713+Artsadthavud@users.noreply.github.com>
2020-08-26 14:54:14 +0700

    เพิ่มคำอธิบายวิธีการเล่นในหน้าเล่นเกม

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 import WordCard from './WordCard';
-import Score from './Score';
 class App extends Component {
    word = wordItems();
  render() {
  return (
  <div className="App">
-   
+   Hello Player !!
+  <br/>
+  Choose the right word to get Score
   <WordCard value={this.word}/>     
-   
+   ** if you need to change word please refresh page
+   <br/>
+   *** if need to see Score you can use console
  </div>
  );
  }
 }
 
 function wordItems() {
-  const reptiles = ["hello", "world", "react"];
-  var rand = 0
-  return reptiles[rand];
+  const stockWord = ["hello", "world", "react","native"];
+  var rand = Math.floor(Math.random() * Math.floor(4));
+  return stockWord[rand];
 }
 export default App;
